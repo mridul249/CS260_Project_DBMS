a5ca8ee1d1949a05b2bf2db36e0058d736816fcf
Mridul Kumar <138252892+mridul249@users.noreply.github.com>
2024-06-28 21:38:48 +0530

    Delete src directory

--- src/components/Blink.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Blink.css";
-function blink() {
-  return (
-    <div className='blinky'><b>Application for Faculty Position</b></div>
-  )
-}
-
-export default blink
